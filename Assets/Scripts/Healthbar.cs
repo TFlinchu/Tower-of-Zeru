fdbd659a220c3ba9a9a95692d34a001a69543017
Tyler Flinchum <tflinchu@emich.edu>
2024-03-07 11:31:01 -0500

    add: updating health Bar
    created a health bar, and has it update when the player takes damage
    
    Issue #15

---
+++ Assets/Scripts/Healthbar.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthbar : MonoBehaviour
+{
+    [SerializeField] private Slider healthSlider;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    public void SetMaxHealth(int maxHealth)
+    {
+        healthSlider.maxValue = maxHealth;
+        healthSlider.value = maxHealth;
+    }
+
+    public void SetHealth(int health)
+    {
+        healthSlider.value = health;
+    }
+}
