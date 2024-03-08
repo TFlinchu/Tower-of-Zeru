d2a862f0be1e61b510af4c699773cdee2d7dba66
Tyler Flinchum <tflinchu@emich.edu>
2024-03-07 20:45:05 -0500

    feature: hp in text on bar
    Added  the hp in text on the Healthbar

--- Assets/Scripts/Healthbar.cs
+++ Assets/Scripts/Healthbar.cs
@@ -2,24 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Healthbar : MonoBehaviour
 {
     [SerializeField] private Slider healthSlider;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
+    [SerializeField] private TextMeshProUGUI healthText;
 
     public void SetMaxHealth(int maxHealth)
     {
         healthSlider.maxValue = maxHealth;
         healthSlider.value = maxHealth;
+        healthText.text = $"{maxHealth}/{healthSlider.maxValue}";
     }
 
     public void SetHealth(int health)
     {
         healthSlider.value = health;
+        healthText.text = $"{health}/{healthSlider.maxValue}";
     }
 }
