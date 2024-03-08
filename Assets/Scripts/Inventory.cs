a3d3a8bdd47ae1c1672a9489eda0f4a58f0d8213
Tyler Flinchum <tflinchu@emich.edu>
2024-03-08 16:16:57 -0500

    Add: Ability to pick up Spells
    Added the ability to pick up spells, with a test one in place.
    UI will be added in the spell inventory branch.

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    // singleton pattern, to be called from other scripts
+    public static Inventory instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // will be used to update inventory UI
+    public delegate void OnItemChanged();
+    public OnItemChanged onItemChangedCallback;
+
+    // basic inventory space
+    public int space = 10;
+    public List<Item> items = new List<Item>();
+
+    // adds an item (only spells for now) to your inventory
+    public bool AddItem(Item item)
+    {
+        // inventory is full
+        if (items.Count >= space)
+        {
+            Debug.Log("Not enough room.");
+            return false;
+        }
+        // inventory has room
+        items.Add(item);
+        // checks if there is a callback, then invokes it
+        onItemChangedCallback?.Invoke();
+        return true;
+    }
+
+    // removes an item from your inventory
+    public void RemoveItem(Item item)
+    {
+        items.Remove(item);
+        // drop item
+    }
+
+}
