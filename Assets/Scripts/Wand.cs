cab607e0f48f92948a51ae053388880bb6f91239
Tyler Flinchum <tflinchu@emich.edu>
2024-03-08 18:09:17 -0500

    stopped player from attacking when hovering over inventory

--- Assets/Scripts/Wand.cs
+++ Assets/Scripts/Wand.cs
@@ -1,5 +1,5 @@
 using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.EventSystems;
 using UnityEngine;
 
 public class Wand : MonoBehaviour
@@ -13,6 +13,9 @@ public class Wand : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (EventSystem.current.IsPointerOverGameObject()) {
+            return;
+        }
         if (Input.GetMouseButton(0) && canAttack)
         {
             if (shootingCoroutine == null)
